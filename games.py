a5ac596fe290f877f9305fce2244ca28f5a9f6ca
kasper589 <daquavisnapalion@gmail.com>
2026-07-16 23:14:50 +0500

    Update games.py

--- games.py
+++ games.py
@@ -19,40 +19,7 @@ GAMES = {
                 "usd": 10
             }
         ]
-    },
-
-    "pubg": {
-        "name": "🟦 PUBG Mobile",
-        "packages": [
-            {
-                "name": "60 UC",
-                "usd": 1
-            },
-            {
-                "name": "325 UC",
-                "usd": 5
-            },
-            {
-                "name": "660 UC",
-                "usd": 10
-            }
-        ]
-    },
-
-    "mobile_legends": {
-        "name": "⚔️ Mobile Legends",
-        "packages": [
-            {
-                "name": "100 Diamond",
-                "usd": 1
-            },
-            {
-                "name": "500 Diamond",
-                "usd": 5
-            }
-        ]
     }
 }
-
 
 USD_RATE = 12500
