6f51d23f1d8962c65b276d1697f9a9209511aab4
kasper589 <daquavisnapalion@gmail.com>
2026-07-16 21:02:00 +0500

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,51 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def donate_menu():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="💳 Karta orqali",
+                    callback_data="card"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="🪙 USDT orqali",
+                    callback_data="usdt"
+                )
+            ]
+        ]
+    )
+
+
+def usdt_amount_menu():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="10 USDT",
+                    callback_data="usdt_10"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="25 USDT",
+                    callback_data="usdt_25"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="50 USDT",
+                    callback_data="usdt_50"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="100 USDT",
+                    callback_data="usdt_100"
+                )
+            ]
+        ]
+    )
