28fa42d1b3c7a90462adda121edd05bb8e70d9a7
kasper589 <daquavisnapalion@gmail.com>
2026-07-16 21:46:19 +0500

    Create games.py

---
+++ games.py
@@ -0,0 +1,58 @@
+GAMES = {
+    "free_fire": {
+        "name": "🔥 Free Fire",
+        "packages": [
+            {
+                "name": "100 Diamond",
+                "usd": 1
+            },
+            {
+                "name": "310 Diamond",
+                "usd": 3
+            },
+            {
+                "name": "520 Diamond",
+                "usd": 5
+            },
+            {
+                "name": "1060 Diamond",
+                "usd": 10
+            }
+        ]
+    },
+
+    "pubg": {
+        "name": "🟦 PUBG Mobile",
+        "packages": [
+            {
+                "name": "60 UC",
+                "usd": 1
+            },
+            {
+                "name": "325 UC",
+                "usd": 5
+            },
+            {
+                "name": "660 UC",
+                "usd": 10
+            }
+        ]
+    },
+
+    "mobile_legends": {
+        "name": "⚔️ Mobile Legends",
+        "packages": [
+            {
+                "name": "100 Diamond",
+                "usd": 1
+            },
+            {
+                "name": "500 Diamond",
+                "usd": 5
+            }
+        ]
+    }
+}
+
+
+USD_RATE = 12500
